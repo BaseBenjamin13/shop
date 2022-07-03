aba4003ebf54aaea2424850e39c2a49ce33df293
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-07-02 21:19:54 -0400

    styling detailed page

--- src/components/DetailedResult.js
+++ src/components/DetailedResult.js
@@ -1,17 +1,25 @@
 import React from 'react'
 
 function DetailedResult({ item }) {
-  return (
-    <div>
-        <h1>{item.title}</h1>
-        <div>
+  return ( 
+    <div className="detailed-container">
+        <h1 className="detailed-title">{item.title}</h1>
+        <div className="detailed-img-div">
             {/* Carousel */}
             {/* map images inside carousel */}
-            <img src={item.image_urls[0]} />
-            <h2>Brand: {item.brand}</h2>
+            <img className="detailed-img" src={item.image_urls[0]} />
         </div>
 
-        <div>{item.description}</div>
+        <div className="info-container">
+            <h2 className="brand">Brand: {item.brand}</h2>
+            <h2 className="detailed-price" >${item.price}</h2>
+        </div>
+
+        <div className="description-container">
+            <p>
+                {item.description}
+            </p>
+        </div>
     </div>
   )
 }

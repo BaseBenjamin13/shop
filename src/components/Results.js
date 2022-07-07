c19dfe79cda43b0f4191f032fee58157003f67f9
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-07-07 18:16:51 -0400

    fixed mapping error

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,9 +5,9 @@ function Results({ items, url}) {
   return (
     <div className="results">
         {
-            items.map(item => {
+            items.map((item, i) => {
                 return(                        
-                    <div className="items">
+                    <div key={i} className="items">
                         <Link className="link" to={url + item.id}>
                             {item.title ? <h2>{item.title.slice(0,30)}</h2> : <h1>title not found</h1>}
                             {item.image_urls && <img className="img" src={item.image_urls[0]} />}

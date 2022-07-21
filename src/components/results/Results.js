15095bf0e044046008ccf0a0b0a82c3f6e1898a7
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-07-21 16:38:49 -0400

    made category/results page mobile responsive

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -6,19 +6,21 @@ function Results({ items, url}) {
     <div className="results">
         {
             items.map((item, i) => {
-                return(                        
-                    <div key={i} className="items">
-                        <Link className="link" to={url + item.id}>
-                            {item.title ? <h2>{item.title.slice(0,30)}</h2> : <h1>title not found</h1>}
-                            {item.image_urls && <img className="img" src={item.image_urls[0]} />}
-                        </Link>
-                        
-                        <div className="price">
-                            <h3>${item.price}</h3>
-                        </div>
+                return(  
+                    <div className="results-container">
+                        <div key={i} className="items">
+                            <Link className="link" to={url + item.id}>
+                                {item.title ? <h2>{item.title.slice(0,30)}</h2> : <h1>title not found</h1>}
+                                {item.image_urls && <img className="img" src={item.image_urls[0]} />}
+                            </Link>
+                            
+                            <div className="price">
+                                <h3>${item.price}</h3>
+                            </div>
 
-                        <button className="cart-btn">Add to Cart</button>
-                    </div>    
+                            <button className="cart-btn">Add to Cart</button>
+                        </div>    
+                    </div>                      
                 )
             })
         }
